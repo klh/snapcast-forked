5758e96fcc114fd4cac9da268706984d1806b20a
Klaus L. Hougesen <klaus.hougesen@gmail.com>
2025-05-03 23:29:40 +0200

    Update message.hpp
    removed comment, just confuses

--- common/message/message.hpp
+++ common/message/message.hpp
@@ -110,7 +110,6 @@ struct tv
     tv()
     {
         timeval t;
-        // Always use steady clock for time synchronization to avoid timezone issues
         chronos::steadytimeofday(&t);
         sec = t.tv_sec;
         usec = t.tv_usec;
