6d50270c3993ba1716bd79944de7aa15b47127cc
Klaus L. Hougesen <klaus.hougesen@gmail.com>
2025-05-12 15:53:48 +0200

    coplete rewrtie of chrony setup

--- common/time_sync.hpp
+++ common/time_sync.hpp
@@ -21,6 +21,7 @@
 // standard headers
 #include <cstdint>
 #include <map>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -129,6 +130,150 @@ std::string timeSourceToString(TimeSyncSource source);
  * @return The corresponding TimeSyncSource enum value
  */
 TimeSyncSource intToTimeSource(int source_int);
+
+/**
+ * Structure to hold chrony tracking information
+ */
+struct ChronyTrackingInfo {
+    double ref_time = 0.0;          ///< Reference timestamp
+    double system_time = 0.0;       ///< System time
+    double last_offset = 0.0;       ///< Last offset (ms)
+    double rms_offset = 0.0;        ///< RMS offset (ms)
+    double freq_ppm = 0.0;          ///< Frequency (ppm)
+    double residual_freq_ppm = 0.0; ///< Residual frequency (ppm)
+    double skew_ppm = 0.0;          ///< Skew (ppm)
+    double root_delay = 0.0;        ///< Root delay (ms)
+    double root_dispersion = 0.0;   ///< Root dispersion (ms)
+    int64_t update_interval = 0;    ///< Update interval (seconds)
+    std::string ref_id;            ///< Reference ID
+    std::string ref_source;         ///< Reference source
+    std::string stratum;           ///< Stratum
+    std::string state;             ///< Synchronization state
+    
+    /**
+     * Parse tracking information from chronyc output
+     * @param tracking_output Output from chronyc tracking command
+     * @return ChronyTrackingInfo structure with parsed data
+     */
+    static ChronyTrackingInfo parse(const std::string& tracking_output) {
+        ChronyTrackingInfo info;
+        
+        // Parse the tracking output line by line
+        std::istringstream iss(tracking_output);
+        std::string line;
+        
+        while (std::getline(iss, line)) {
+            // Skip empty lines
+            if (line.empty()) continue;
+            
+            // Parse key-value pairs
+            size_t colon_pos = line.find(":");
+            if (colon_pos != std::string::npos) {
+                std::string key = line.substr(0, colon_pos);
+                std::string value = line.substr(colon_pos + 1);
+                
+                // Trim whitespace
+                key.erase(0, key.find_first_not_of(" \t"));
+                key.erase(key.find_last_not_of(" \t") + 1);
+                value.erase(0, value.find_first_not_of(" \t"));
+                value.erase(value.find_last_not_of(" \t") + 1);
+                
+                // Fill in the appropriate field based on the key
+                if (key == "Reference ID") info.ref_id = value;
+                else if (key == "Stratum") info.stratum = value;
+                else if (key == "Ref time (UTC)") {
+                    // Parse timestamp if needed
+                }
+                else if (key == "System time") {
+                    // Extract the offset value
+                    size_t offset_pos = value.find("seconds");
+                    if (offset_pos != std::string::npos) {
+                        std::string offset_str = value.substr(0, offset_pos);
+                        try {
+                            info.system_time = std::stod(offset_str);
+                        } catch (...) {}
+                    }
+                }
+                else if (key == "Last offset") {
+                    size_t unit_pos = value.find("seconds");
+                    if (unit_pos != std::string::npos) {
+                        std::string offset_str = value.substr(0, unit_pos);
+                        try {
+                            info.last_offset = std::stod(offset_str) * 1000.0; // Convert to ms
+                        } catch (...) {}
+                    }
+                }
+                else if (key == "RMS offset") {
+                    size_t unit_pos = value.find("seconds");
+                    if (unit_pos != std::string::npos) {
+                        std::string offset_str = value.substr(0, unit_pos);
+                        try {
+                            info.rms_offset = std::stod(offset_str) * 1000.0; // Convert to ms
+                        } catch (...) {}
+                    }
+                }
+                else if (key == "Frequency") {
+                    size_t unit_pos = value.find("ppm");
+                    if (unit_pos != std::string::npos) {
+                        std::string freq_str = value.substr(0, unit_pos);
+                        try {
+                            info.freq_ppm = std::stod(freq_str);
+                        } catch (...) {}
+                    }
+                }
+                else if (key == "Residual freq") {
+                    size_t unit_pos = value.find("ppm");
+                    if (unit_pos != std::string::npos) {
+                        std::string freq_str = value.substr(0, unit_pos);
+                        try {
+                            info.residual_freq_ppm = std::stod(freq_str);
+                        } catch (...) {}
+                    }
+                }
+                else if (key == "Skew") {
+                    size_t unit_pos = value.find("ppm");
+                    if (unit_pos != std::string::npos) {
+                        std::string skew_str = value.substr(0, unit_pos);
+                        try {
+                            info.skew_ppm = std::stod(skew_str);
+                        } catch (...) {}
+                    }
+                }
+                else if (key == "Root delay") {
+                    size_t unit_pos = value.find("seconds");
+                    if (unit_pos != std::string::npos) {
+                        std::string delay_str = value.substr(0, unit_pos);
+                        try {
+                            info.root_delay = std::stod(delay_str) * 1000.0; // Convert to ms
+                        } catch (...) {}
+                    }
+                }
+                else if (key == "Root dispersion") {
+                    size_t unit_pos = value.find("seconds");
+                    if (unit_pos != std::string::npos) {
+                        std::string disp_str = value.substr(0, unit_pos);
+                        try {
+                            info.root_dispersion = std::stod(disp_str) * 1000.0; // Convert to ms
+                        } catch (...) {}
+                    }
+                }
+                else if (key == "Update interval") {
+                    size_t unit_pos = value.find("seconds");
+                    if (unit_pos != std::string::npos) {
+                        std::string interval_str = value.substr(0, unit_pos);
+                        try {
+                            info.update_interval = std::stoll(interval_str);
+                        } catch (...) {}
+                    }
+                }
+                else if (key == "Leap status") info.state = value;
+                else if (key == "Reference source") info.ref_source = value;
+            }
+        }
+        
+        return info;
+    }
+};
 
 /**
  * Structure to hold time information from a specific source
